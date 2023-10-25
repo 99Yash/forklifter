c7a8e15f34b4e59b1bc3c6db54fb0a8c48abe2fe
yash <yashgouravkar@gmail.com>
2023-10-25 10:21:53 +0530

    fix: removes shitty dependencies

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,4 +1,3 @@
-import { User } from "@clerk/nextjs/server";
 import * as Icons from "@/components/ui/icons";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
@@ -13,17 +12,12 @@ import {
 } from "./ui/dropdown-menu";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { type User } from "next-auth";
+import { extractInitials } from "@/lib/utils";
 
 export function UserNav({ user }: { user: User }) {
-  const initials = `${user?.firstName?.charAt(0) ?? ""} ${
-    user?.lastName?.charAt(0) ?? ""
-  }`;
   function getUserEmail(user: User) {
-    const email =
-      user?.emailAddresses?.find((e) => e.id === user.primaryEmailAddressId)
-        ?.emailAddress ?? "";
-
-    return email;
+    return user.email;
   }
   const email = getUserEmail(user);
   return (
@@ -79,17 +73,15 @@ export function UserNav({ user }: { user: User }) {
       <DropdownMenuTrigger asChild>
         <Button variant="secondary" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src={user.imageUrl} alt={user.username ?? ""} />
-            <AvatarFallback>{initials}</AvatarFallback>
+            <AvatarImage src={user.image!} alt={user.username ?? ""} />
+            <AvatarFallback>{extractInitials(user.name!)}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {user.firstName} {user.lastName}
-            </p>
+            <p className="text-sm font-medium leading-none">{user.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
               {email}
             </p>
