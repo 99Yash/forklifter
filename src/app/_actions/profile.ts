f33957cb14603a56a94961dc152d30d62395bf4b
yash <yashgouravkar@gmail.com>
2024-01-05 13:15:59 +0530

    better validation username

--- src/app/_actions/profile.ts
+++ src/app/_actions/profile.ts
@@ -1,12 +1,21 @@
-"use server";
+'use server';
 
-import { getCurrentUser } from "@/lib/auth-opts";
-import { prisma } from "@/lib/db";
-import { profileFormSchema } from "@/lib/schemas";
-import { revalidatePath } from "next/cache";
-import { z } from "zod";
+import { getCurrentUser } from '@/lib/auth-opts';
+import { prisma } from '@/lib/db';
+import { profileFormSchema } from '@/lib/schemas';
+import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
 
-export async function updateProfile({bio,displayName,linkedinUrl,oneLiner,email,githubUrl,twitterUrl,username}: z.infer<typeof profileFormSchema>) {
+export async function updateProfile({
+  bio,
+  displayName,
+  linkedinUrl,
+  oneLiner,
+  email,
+  githubUrl,
+  twitterUrl,
+  username,
+}: z.infer<typeof profileFormSchema>) {
   const user = await getCurrentUser();
   if (!user) throw new Error("You're not authenticated.");
 
@@ -16,7 +25,22 @@ export async function updateProfile({bio,displayName,linkedinUrl,oneLiner,email,
     },
   });
 
-  if (!dbUser) throw new Error("User not found");
+  if (!dbUser) throw new Error('User not found');
+
+  if (dbUser.username !== username) {
+    if (username === 'dashboard')
+      throw new Error('Username cannot be set as "dashboard"');
+
+    const usernameExists = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (usernameExists) {
+      throw new Error(`Username ${username} already exists`);
+    }
+  }
 
   await prisma.user.update({
     data: {
@@ -27,11 +51,11 @@ export async function updateProfile({bio,displayName,linkedinUrl,oneLiner,email,
       linkedinUrl,
       twitterUrl,
       githubUrl,
-      bio
+      bio,
     },
-    where:{
-      id:user.id
-    }
+    where: {
+      id: user.id,
+    },
   });
   revalidatePath(`/dashboard`);
 }
