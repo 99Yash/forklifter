114073bdb3ac35aeaee1ed5190f40309ef82ec95
yash <yashgouravkar@gmail.com>
2024-02-10 13:15:45 +0530

    secure analytics aciton

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -5,6 +5,7 @@ import { type Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import InsightCard from './card';
 import Link from 'next/link';
+import { fetchAnalytics } from '@/app/_actions/analytics';
 
 export const metadata: Metadata = {
   title: 'Analytics',
@@ -15,89 +16,91 @@ const page = async () => {
   const user = await getCurrentUser();
   if (!user) return redirect('/');
 
-  const { data: insights } = await axios.get<{
-    insight: {
-      uniqueVisitors: {
-        current: number;
-        change: number; //this is a change from comparable time range in the past.
-      };
-      totalPageViews: {
-        current: number;
-        change: number;
-      };
-      averageTime: {
-        current: string;
-        change: number;
-      };
-      bounceRate: {
-        current: number;
-        change: number;
-      };
-      newVisitors: {
-        current: number;
-        change: number;
-      };
-      returningVisitor: {
-        current: number;
-        change: number;
-      };
-    };
-    data: {
-      pages: {
-        page: string;
-        visits: number;
-      }[];
-      devices: {
-        device: string;
-        visits: number;
-      }[];
-      referrer: {
-        referrer: string;
-        visits: number;
-        referrerDomain: string;
-      }[];
-      locations: {
-        city: {
-          location: string;
-          visits: number;
-          country: string;
-        }[];
-        country: {
-          location: string;
-          visits: number;
-        }[];
-      };
-      os: {
-        os: string;
-        visits: number;
-      }[];
-      browser: {
-        browser: string;
-        visits: number;
-      }[];
-      utmSources: {
-        utmSource: string;
-        visits: number;
-      }[];
-      utmCampaigns: {
-        utmCampaign: string;
-        visits: number;
-      }[];
-      onlineVisitors: number;
-    };
-    graph: {
-      uniqueVisitorsByDate: {
-        date: string;
-        visits: number;
-      }[];
-      uniqueSessionByDate: {
-        date: string;
-        visits: number;
-      }[];
-    };
-  }>(`https://api.loglib.io/v1/insight?apiKey=${env.LOGLIB_API_KEY}`, {
-    withCredentials: false,
-  });
+  const insights = await fetchAnalytics();
+
+  // const { data: insights } = await axios.get<{
+  //   insight: {
+  //     uniqueVisitors: {
+  //       current: number;
+  //       change: number; //this is a change from comparable time range in the past.
+  //     };
+  //     totalPageViews: {
+  //       current: number;
+  //       change: number;
+  //     };
+  //     averageTime: {
+  //       current: string;
+  //       change: number;
+  //     };
+  //     bounceRate: {
+  //       current: number;
+  //       change: number;
+  //     };
+  //     newVisitors: {
+  //       current: number;
+  //       change: number;
+  //     };
+  //     returningVisitor: {
+  //       current: number;
+  //       change: number;
+  //     };
+  //   };
+  //   data: {
+  //     pages: {
+  //       page: string;
+  //       visits: number;
+  //     }[];
+  //     devices: {
+  //       device: string;
+  //       visits: number;
+  //     }[];
+  //     referrer: {
+  //       referrer: string;
+  //       visits: number;
+  //       referrerDomain: string;
+  //     }[];
+  //     locations: {
+  //       city: {
+  //         location: string;
+  //         visits: number;
+  //         country: string;
+  //       }[];
+  //       country: {
+  //         location: string;
+  //         visits: number;
+  //       }[];
+  //     };
+  //     os: {
+  //       os: string;
+  //       visits: number;
+  //     }[];
+  //     browser: {
+  //       browser: string;
+  //       visits: number;
+  //     }[];
+  //     utmSources: {
+  //       utmSource: string;
+  //       visits: number;
+  //     }[];
+  //     utmCampaigns: {
+  //       utmCampaign: string;
+  //       visits: number;
+  //     }[];
+  //     onlineVisitors: number;
+  //   };
+  //   graph: {
+  //     uniqueVisitorsByDate: {
+  //       date: string;
+  //       visits: number;
+  //     }[];
+  //     uniqueSessionByDate: {
+  //       date: string;
+  //       visits: number;
+  //     }[];
+  //   };
+  // }>(`https://api.loglib.io/v1/insight?apiKey=${env.LOGLIB_API_KEY}`, {
+  //   withCredentials: false,
+  // });
 
   return (
     <div className="flex flex-col gap-4">
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
-import { getCurrentUser } from "@/lib/auth-opts";
-import { redirect } from "next/navigation";
-import Header from "./header";
-import { SidebarNav } from "./sidebar";
+import { getCurrentUser } from '@/lib/auth-opts';
+import { redirect } from 'next/navigation';
+import Header from './header';
+import { SidebarNav } from './sidebar';
 
 export default async function Layout({
   children,
@@ -9,14 +9,14 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   const user = await getCurrentUser();
-  if (!user) redirect("/");
+  if (!user) redirect('/');
 
   return (
     <div className="flex h-screen flex-col gap-4 bg-zinc-950/10">
       <Header user={user} />
       <hr className="max-w-screen mx-3 -mt-4 h-px border-0 bg-secondary" />
       <div className="container mt-1 flex flex-1 gap-8">
-        <aside className="hidden md:flex md:w-52">
+        <aside className="hidden lg:flex lg:w-52">
           <SidebarNav />
         </aside>
         <main className="flex flex-1 flex-col overflow-hidden">{children}</main>
