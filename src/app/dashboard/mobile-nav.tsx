d9d744808420aa8b793b8915f906fd936b1ddc96
yash <yashgouravkar@gmail.com>
2024-02-10 19:44:38 +0530

    added styles

--- src/app/dashboard/mobile-nav.tsx
+++ src/app/dashboard/mobile-nav.tsx
@@ -17,31 +17,33 @@ export default function MobileNav() {
       <SheetTrigger asChild>
         <Button
           variant="ghost"
-          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden"
+          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 lg:hidden text-muted-foreground"
         >
-          <Icons.Columns className="size-6" aria-hidden="true" />
+          <Icons.Columns className="h-5 w-5" aria-hidden="true" />
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="pl-1 pr-0">
-        <div className="px-7">
-          <Link
-            href="/"
-            className="flex items-center"
-            onClick={() => setIsOpen(false)}
-          >
-            <Icons.Logo className="mr-2 size-4" aria-hidden="true" />
-            <span className="font-bold">{siteConfig.name}</span>
-            <span className="sr-only">Home</span>
-          </Link>
-        </div>
-        <div className="pl-1 pr-7">
-          <div className="flex flex-col space-y-2">
+        <div className="px-7 flex flex-col gap-4 h-full">
+          <div className="flex gap-2 items-center">
+            <Link
+              href="/"
+              className="flex items-center"
+              onClick={() => setIsOpen(false)}
+            >
+              <Icons.Logo className="mr-2 h-5 w-5" aria-hidden="true" />
+              <span className="font-bold">{siteConfig.name}</span>
+              <span className="sr-only">Home</span>
+            </Link>
+          </div>
+          <div className="flex flex-col gap-4 mt-4">
             {workspaceItems?.map((item, index) =>
               item.href ? (
                 <Link
                   key={index}
-                  href={item.href}
+                  href={
+                    item.href === '/' ? '/dashboard' : `/dashboard${item.href}`
+                  }
                   className={cn(
                     'text-foreground/70 transition-colors hover:text-foreground'
                   )}
