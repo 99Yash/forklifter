c54ef4038453ec678a2362888e408ef57d2448d8
yash <yashgouravkar@gmail.com>
2024-05-06 08:32:17 +0530

    experiences

--- src/app/_actions/experience.ts
+++ src/app/_actions/experience.ts
@@ -31,3 +31,53 @@ export async function addExperience(input: z.infer<typeof experienceSchema>) {
   revalidatePath(`/dashboard/experiences`);
   revalidatePath(`/${dbUser.username}`);
 }
+
+export async function updateExperience(
+  id: string,
+  input: z.infer<typeof experienceSchema>
+) {
+  const user = await getCurrentUser();
+  if (!user) throw new Error("You're not authenticated.");
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      id: user?.id,
+    },
+  });
+  if (!dbUser) throw new Error('User not found');
+
+  await prisma.experience.update({
+    where: {
+      id,
+    },
+    data: {
+      currentlyWorking: input.endDate ? false : true,
+      description: input.description,
+      orgName: input.orgName,
+      startDate: input.startDate ?? new Date(),
+      endDate: input.endDate,
+      position: input.position,
+      orgUrl: input.orgUrl,
+    },
+  });
+  revalidatePath(`/dashboard/experiences`);
+  revalidatePath(`/${dbUser.username}`);
+}
+
+export async function deleteExperience(id: string) {
+  const user = await getCurrentUser();
+  if (!user) throw new Error("You're not authenticated.");
+  const dbUser = await prisma.user.findUnique({
+    where: {
+      id: user?.id,
+    },
+  });
+  if (!dbUser) throw new Error('User not found');
+
+  await prisma.experience.delete({
+    where: {
+      id,
+    },
+  });
+  revalidatePath(`/dashboard/experiences`);
+  revalidatePath(`/${dbUser.username}`);
+}
