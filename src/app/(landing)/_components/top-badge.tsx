81c9a3bd1295c90b2f645b69ea5f3fc8822568c1
yash <yashgouravkar@gmail.com>
2024-05-13 07:01:44 +0530

    link typo

--- src/app/(landing)/_components/top-badge.tsx
+++ src/app/(landing)/_components/top-badge.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Badge = () => {
   return (
-    <Link href={'https://devfolio-client.vercel.app/'} target="_blank">
+    <Link href={siteConfig.links.github} target="_blank">
       <h6 className="group inline-flex h-full animate-background-shine cursor-pointer items-center justify-center rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000,45%,#7a7c7995,55%,#000)] bg-[length:250%_100%] px-3 py-1 text-xs font-medium backdrop-blur-3xl md:text-sm">
         <span className="bg-gradient-to-r from-gray-300 via-gray-100 to-gray-300 bg-clip-text px-1 py-px text-xs font-semibold text-transparent duration-300 hover:text-gray-200">
           {siteConfig.name} is Open Source
