266b5f140ac4929e5c04a157f07e6553762efa75
yash <yashgouravkar@gmail.com>
2024-06-18 11:26:23 +0530

    alt

--- src/app/(website)/not-found.tsx
+++ src/app/(website)/not-found.tsx
@@ -18,7 +18,7 @@ const NotFound = () => {
         height={550}
         className="absolute left-1/2 top-0 -z-10 -translate-x-1/2"
         src="/images/gradient-background-top.png"
-        alt=""
+        alt="Gradient background"
         role="presentation"
         priority
       />
